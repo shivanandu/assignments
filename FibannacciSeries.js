776b2a93a293aab2f597ac5199ff3a16df647b4c
shivanandu <shivanandureddy@gmail.com>
2016-03-25 17:13:16 -0400

    Added files via upload

---
+++ FibannacciSeries.js
@@ -0,0 +1,13 @@
+﻿function Fibanacci(digit) {
+    var a = 0, b = 1;
+    console.log(a);
+    console.log(b);
+    for (var i = 2; i <= digit; i++) {
+        var sum = a + b;
+        console.log(sum);
+        a = b;
+        b = sum;  
+    }
+}
+Fibanacci(10);
+Fibanacci(9);
